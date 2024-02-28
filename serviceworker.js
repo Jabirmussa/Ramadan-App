c03e7612aabb8eaa613232b3b27d9b5b908423f4
jabirmussa <jabirmussa185@gmail.com>
2024-02-28 20:43:37 +0200

    jsssss

--- serviceworker.js
+++ serviceworker.js
@@ -1,45 +1,65 @@
 var staticCacheName = "pwa-v1";
 
+// Função para abrir o banco de dados
+function openDatabase() {
+  return new Promise(function(resolve, reject) {
+    var request = indexedDB.open('pwa-database', 1);
+
+    request.onerror = function(event) {
+      reject('Erro ao abrir o banco de dados: ' + event.target.errorCode);
+    };
+
+    request.onsuccess = function(event) {
+      resolve(request.result);
+    };
+
+    request.onupgradeneeded = function(event) {
+      var db = event.target.result;
+      var objectStore = db.createObjectStore('data', { keyPath: 'id' });
+      objectStore.createIndex('name', 'name', { unique: false });
+      console.log('Banco de dados criado com sucesso.');
+    };
+  });
+}
+
 self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/main.html',
-        '/sobre.html',
-        '/styles.css',
-        '/js/app.js',
-        // Adicione outros recursos que você deseja armazenar em cache aqui
-      ]);
-    })
+    openDatabase() // Abre o banco de dados ao instalar o Service Worker
+      .then(function(db) {
+        // Adicione os recursos ao cache
+        return caches.open(staticCacheName).then(function(cache) {
+          return cache.addAll([
+            '/',
+            '/index.html',
+            '/main.html',
+            '/styles.css',
+            '/js/app.js',
+            // Adicione outros recursos que você deseja armazenar em cache aqui
+          ]);
+        });
+      })
   );
 });
 
 self.addEventListener("fetch", function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      // Verifica se a resposta está no cache
       if (response) {
-        return response; // Retorna a resposta do cache
+        return response;
       }
 
-      // Caso contrário, faz a solicitação à rede
       return fetch(event.request).then(function(response) {
-        // Verifica se a resposta é válida e se é necessário armazenar em cache
         if (!response || response.status !== 200 || response.type !== "basic") {
-          return response; // Retorna a resposta normal da rede
+          return response;
         }
 
-        // Clona a resposta para que ela possa ser consumida pela requisição e pelo cache
         var responseToCache = response.clone();
 
-        // Abre o cache e armazena a resposta clonada
         caches.open(staticCacheName).then(function(cache) {
           cache.put(event.request, responseToCache);
         });
 
-        return response; // Retorna a resposta normal da rede
+        return response;
       });
     })
   );
